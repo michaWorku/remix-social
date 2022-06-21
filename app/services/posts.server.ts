57e004aa37cd7e9d3111ffc485beb092096748a8
mikias.wk <mikias.wk@2f-capital.com>
2022-06-21 22:09:23 +0300

    add postform, db configured with pscale

--- app/services/posts.server.ts
+++ app/services/posts.server.ts
@@ -1,7 +1,12 @@
+import { Post } from '@prisma/client'
 import { db } from './db.server'
 
 export type { Post } from '@prisma/client'
 
 export const getPosts = () =>{
     return db.post.findMany()
+}
+
+export const createPost = ({title, body}: Pick<Post, 'title' | 'body'>) =>{
+    return db.post.create({data:{title, body}})
 }
