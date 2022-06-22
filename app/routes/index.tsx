33cc57e3d5bbcd3668365e1418aadde90ed10615
mikias.wk <mikias.wk@2f-capital.com>
2022-06-22 09:34:42 +0300

    validate form data through remix action using zod

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,48 @@
-import { json, LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { ActionFunction, json, LoaderFunction, redirect } from "@remix-run/node";
+import { useActionData, useLoaderData } from "@remix-run/react";
 import { Post } from "~/components/Post";
-import { getPosts } from "~/services/posts.server";
+import { PostForm } from "~/components/PostForm";
+import { createPost, getPosts } from "~/services/posts.server";
+import { CreatePost } from "~/services/validation";
 
 type LoaderData = {
   posts: Awaited<ReturnType<typeof getPosts>>
+}
+
+type ActionData = {
+  error: {
+    formError?: string[]
+    fieldErrors?: {
+      title: string[]
+      body: string[]
+    }
+  }
+  fields: {
+    title: string
+    body: string
+  }
+}
+
+export const action: ActionFunction = async ({request}) => {
+  const form = await request.formData()
+  const rawTitle = form.get('title')
+  const rawBody = form.get('body')
+
+  const result = CreatePost.safeParse({title: rawTitle, body: rawBody})
+
+  if (!result.success) {
+    return json(
+      {error: result.error.flatten(), fields: {title: rawTitle, body: rawBody}},
+      {status: 400},
+    )
+  }
+
+  const post = await createPost({
+    title: result.data.title ?? null,
+    body: result.data.body,
+  })
+
+  return redirect('/')
 }
 
 export const loader: LoaderFunction =async () => {
@@ -15,10 +53,14 @@ export const loader: LoaderFunction =async () => {
 
 export default function Index() {
   const {posts} = useLoaderData() as LoaderData
+  const formData = useActionData() as ActionData
   
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
+      <div className="mb-8">
+        <PostForm method="post" action="/?index" error={formData.error} fields={formData.fields} />
+      </div>
       <ul>
         {
           posts.map((post)=>(
